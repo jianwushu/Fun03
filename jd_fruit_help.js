5a8acf8b8f60934f5ef0ad9a6bbe7497b761824e
haohao <2574652740@qq.com>
2022-11-14 13:40:51 +0800

    add ddnc

--- jd_fruit_help.js
+++ jd_fruit_help.js
@@ -49,7 +49,7 @@ let NoNeedCodes = [];
                 message = '';
                 subTitle = '';
                 option = {};
-                $.UA = require('./USER_AGENTS').UARAM();
+                $.UA = require('./function/USER_AGENTS').UARAM();
                 $.retry = 0;
                 llgetshare = false;
                 await GetCollect();
@@ -360,7 +360,7 @@ function TotalBean() {
                 "Connection": "keep-alive",
                 "Cookie": cookie,
                 "Referer": "https://wqs.jd.com/my/jingdou/my.shtml?sceneval=2",
-                "User-Agent": $.isNode() ? (process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : (require('./USER_AGENTS').USER_AGENT)) : ($.getdata('JDUA') ? $.getdata('JDUA') : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1")
+                "User-Agent": $.isNode() ? (process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : (require('./function/USER_AGENTS').USER_AGENT)) : ($.getdata('JDUA') ? $.getdata('JDUA') : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1")
             }
         }
         $.post(options, (err, resp, data) => {
