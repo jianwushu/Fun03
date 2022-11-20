28ed7aaddd3d99ef24336e97c50e07184565f7b2
haohao <2574652740@qq.com>
2022-11-20 23:58:40 +0800

    upd jdlite_park

--- jdlite_park_help.js
+++ jdlite_park_help.js
@@ -133,6 +133,13 @@ function taskPostClientActionUrl(body, functionId) {
     }
   }
 }
+function randomString(e) {
+  e = e || 32;
+  let t = "abcdef0123456789", a = t.length, n = "";
+  for (i = 0; i < e; i++)
+    n += t.charAt(Math.floor(Math.random() * a));
+  return n
+}
 
 function jsonParse(str) {
   if (typeof str == "string") {
